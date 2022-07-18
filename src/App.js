20592336271aac7a8c621074220014d4f02d387b
AlexPap <arpappagallo@gmail.com>
2022-07-18 14:18:24 -0400

    added page functionality in react. Populate proper info per page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Portfolio from "./components/pages/Portfolio";
-import Contact from "./components/pages/Contact";
-import Resume from "./components/pages/Resume";
+import PortfolioContainer from "./components/PortfolioContainer";
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <NavTabs />
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/resume" component={Resume} />
-      </div>
-    </Router>
-  );
-}
+const App = () => <PortfolioContainer />;
 
 export default App;
