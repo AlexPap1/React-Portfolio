ae0cf8e142e88128ba346b5022597a4f87a9f43d
AlexPap <arpappagallo@gmail.com>
2022-07-18 12:10:34 -0400

    boilerplate react setup with files created for src folder. will populate next

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+import Resume from "./components/pages/Resume";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/resume" component={Resume} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
